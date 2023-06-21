d83adc2fea9001d6df42118cf3069cbbe8f20eae
marotella <michelle.a.rotella@gmail.com>
2023-06-21 17:15:47 -0400

    Initialized express

---
+++ about.js
@@ -0,0 +1,7 @@
+{
+    "name":"Michelle Rotella",
+    "email": "michelle.a.rotella@gmail.com",
+    "headshot": "http://www.imgur.com/pictureofproject.png",
+    "bio": "Michelle graduated from General Assembly in 2023. Prior to General Assembly, Michelle discovered she enjoyed programming during the pandemic and enrolled in a prep course with Hackbright Academy. She has extensive professional experience in education, edTech, and consulting. She enjoys tackling complex problems to make doing hard work easier. Currently she is working on learning Typescript."
+  }
+
---
+++ projects.js
@@ -0,0 +1,40 @@
+[
+    {
+      "name": "Two Player Project",
+      "live": "https://app.herokuapp.com/whatever",
+      "git": "http://www.github.com/username/reponame",
+      "image": "http://www.imgur.com/pictureofproject.png",
+      "type":"Solo",
+      "description":"This is a two player game, created around early ninities action stars. This was the first project I created, and helped me to learn DOM manipulation, Javascript, and Vanilla CSS.",
+      "technologies": "HTML/CSS, JavaScript"
+    },
+    {
+        "name": "Relay",
+        "live": "https://app.herokuapp.com/whatever",
+        "git": "http://www.github.com/username/reponame",
+        "image": "http://www.imgur.com/pictureofproject.png",
+        "type":"Solo",
+        "description":"This application was made to support project management. It helped me deepend my understading of Agile and scrum. It helped me to build understanding of routes, components, and EJS.",
+        "technologies": "HTML/CSS, JavaScript, Express, Heroku"
+      },
+      {
+        "name": "Cave-a-vin",
+        "live": "https://app.herokuapp.com/whatever",
+        "git": "http://www.github.com/username/reponame",
+        "image": "http://www.imgur.com/pictureofproject.png",
+        "type":"Group",
+        "description":"This MERN application was created with a team to allow a used to discover new wines. It was my first collaborative project with React,and deepened my understanding of GitHub, Git, and working with external APIs.",
+        "technologies": "HTML/CSS, Bootstrap, JavaScript, Express, React, MongoDB, Heroku"
+      },
+      {
+        "name": "What's for Dinner?",
+        "live": "https://app.herokuapp.com/whatever",
+        "git": "http://www.github.com/username/reponame",
+        "image": "http://www.imgur.com/pictureofproject.png",
+        "type":"Solo",
+        "description":"This Flask-React application was created to dicsover new recipes using the ingredients you have on hand, and reduce food waste. This application deepended my understanding of React, and allowed me to use my knowledge of Python.",
+        "technologies": "HTML/CSS, TailwindCSS, JavaScript, Python, Flask, React, SQL, Heroku"
+      },
+
+  ]
+  
