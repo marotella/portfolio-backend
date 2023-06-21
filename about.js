5eb3bad5080f5fa7a4382dc16d5dd9c9bfd565ca
marotella <michelle.a.rotella@gmail.com>
2023-06-21 17:20:41 -0400

    server set up

--- about.js
+++ about.js
@@ -1,7 +1,9 @@
 {
-    "name":"Michelle Rotella",
+    "name": "Michelle Rotella",
     "email": "michelle.a.rotella@gmail.com",
-    "headshot": "http://www.imgur.com/pictureofproject.png",
-    "bio": "Michelle graduated from General Assembly in 2023. Prior to General Assembly, Michelle discovered she enjoyed programming during the pandemic and enrolled in a prep course with Hackbright Academy. She has extensive professional experience in education, edTech, and consulting. She enjoys tackling complex problems to make doing hard work easier. Currently she is working on learning Typescript."
+    "linkedin": "https://www.linkedin.com/in/michelle-rotella/",
+    "resume": "https://drive.google.com/file/d/1PasAMyxONHap6sGYm8yM4d3fNCI6eWP1/view?usp=share_link",
+    "headshot": "https://drive.google.com/file/d/1T42x1WnhTbvI0M-LZCGF3YQBNI3tD2vW/view?usp=share_link",
+    "bio": "Michelle graduated from General Assembly's Software Engineering program in 2023. Prior to General Assembly, Michelle discovered she enjoyed programming during the pandemic, and enrolled in a prep course with Hackbright Academy. She has extensive professional experience in education, edTech, and consulting. She enjoys tackling complex problems to make doing hard work easier. Currently she is working on learning Typescript."
   }
 
---
+++ server.js
@@ -0,0 +1,40 @@
+//DEPENDENCIES
+const express = require("express")
+const cors = require("cors")
+
+
+//JSON IMPORTS
+const projects = require("./projects.json")
+const about = require("./about.json")
+
+
+//APP
+
+const app = express ()
+
+
+//MIDDLEWARE
+app.use(cors())
+
+
+//HOME ROUTE
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+//PROJECT SHOW ROUTE
+app.get ("/projects", (req,res)=> {
+    res.json(projects)
+})
+
+
+//ROUTE TO SHOW ABOUT
+app.get("/about", (req, res)=> {
+    res.json(about)
+})
+
+//PORT
+const PORT = process.env.PORT || 4000;
+
+//SERVER LISTENER 
+app.listen(PORT, () => console.log("Listening on port  "+ PORT))
