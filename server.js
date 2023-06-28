437f223474347d9406ce6cfb16c5d3337f83125a
marotella <michelle.a.rotella@gmail.com>
2023-06-27 22:56:36 -0400

    contact/email page completed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //DEPENDENCIES
 const express = require("express")
 const cors = require("cors")
-
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 //JSON IMPORTS
 const projects = require("./projects.json")
@@ -10,12 +11,18 @@ const about = require("./about.json")
 
 //APP
 
-const app = express ()
+const app = express()
 
 
 //MIDDLEWARE
 app.use(cors())
-
+app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 //HOME ROUTE
 app.get("/", (req, res) => {
@@ -23,23 +30,26 @@ app.get("/", (req, res) => {
 })
 
 //PROJECT SHOW ROUTE
-app.get ("/projects", (req,res)=> {
+app.get("/projects", (req, res) => {
     res.json(projects)
 })
 
 
 //ROUTE TO SHOW ABOUT
-app.get("/about", (req, res)=> {
+app.get("/about", (req, res) => {
     res.json(about)
 })
 
 //ROUTE TO SHOW Contact
-app.get("/contact", (req, res)=> {
+app.get("/contact", (req, res) => {
     res.json(about)
 })
+;
+module.exports = app;
+
 
 //PORT
 const PORT = process.env.PORT || 4000;
 
 //SERVER LISTENER 
-app.listen(PORT, () => console.log("Listening on port  "+ PORT))
+app.listen(PORT, () => console.log("Listening on port  " + PORT))
